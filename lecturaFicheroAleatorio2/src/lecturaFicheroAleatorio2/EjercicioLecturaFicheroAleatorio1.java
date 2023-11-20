6b409ed0c6c0c6e9714420ad3a31662db0f8d5f7
jgonzalez <jgonzaleze@iesnervion.es>
2023-11-20 11:13:43 +0100

    Ejercicio 1 comentado

--- lecturaFicheroAleatorio2/src/lecturaFicheroAleatorio2/EjercicioLecturaFicheroAleatorio1.java
+++ lecturaFicheroAleatorio2/src/lecturaFicheroAleatorio2/EjercicioLecturaFicheroAleatorio1.java
@@ -11,24 +11,36 @@ public class EjercicioLecturaFicheroAleatorio1 {
 	// aleatoria (seek).
 
 	public static void main(String[] args) {
+		// Try ya que vamos a manejar archivos y pueden ocurrir errores
 		try {
-
+			// Para el texto que vamos a leer
 			File atexto = new File("a.txt");
+			// Para el texto que vamos a escribir
 			File aaaatexto = new File("aaaa.txt");
-
-			RandomAccessFile filea = new RandomAccessFile(atexto, "rw");
+			
+			// Para acceder al texto con RandomAcess con el modo r, solo de lectura
+			RandomAccessFile filea = new RandomAccessFile(atexto, "r");
+			// Para acceder al texto con RandomAcess con el modo rw, lectura y escritura
 			RandomAccessFile fileaaaa = new RandomAccessFile(aaaatexto, "rw");
 			
+			// Usamos seek en la posición que queremos leer, en este caso la primera
 			filea.seek(0);
+			// Metemos en un string la linea que sacamos del archivo
 			String a = filea.readLine();
+			// Cerramos el archivo 
 			filea.close();
+			// Bucle for que iterará las veces que queramos que se escriba la letra
 			for (int i = 0; i < 5; i++) {
 				fileaaaa.writeChars(a);
+				// Print para tener un seguimiento de las veces que se ha impreso
 				System.out.println("Letra a escrita");
 			}
+			// Cerramos el archivo que acabmos de escribir cuando acabe el bucle
 			fileaaaa.close();
+		// Catch para atrapar las excepciones
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.out.println("Error!");
 		}
 	}
 
